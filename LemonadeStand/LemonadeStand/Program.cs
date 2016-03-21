3652c4b7906875339ee963ce2dd8b2769cff2350
jdbogieman <justin.diblasi@gmail.com>
2016-03-21 18:05:04 -0500

    New Lemonade Stand Repository

---
+++ LemonadeStand/LemonadeStand/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the start of my Lemonade Stand");
+        }
+    }
+}
